6d9f1dd5c819f68761a1756a4e724730a75ca974
Michael Crismali <michael.crismali@gmail.com>
2017-10-25 22:25:45 -0500

    remove unused import

--- package/Stack/InnerStack.js
+++ package/Stack/InnerStack.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { Animation } from '../Animation'
 import { SceneWrapper } from './SceneWrapper'
 
